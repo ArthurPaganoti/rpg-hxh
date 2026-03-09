ae0aeb90e8dbb49176f82158b27b523bcd2a1351
Art <paganotiarthur@gmail.com>
2026-03-09 00:49:33 -0300

    :sparkles: feat: initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/test/java/com/rpg/rpghxh/RpgHxhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.rpghxh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgHxhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
