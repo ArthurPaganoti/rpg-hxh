8433c1fc3c53eb70093d25282facb21028e42c13
Art <paganotiarthur@gmail.com>
2026-03-09 19:50:04 -0300

    :sparkles: feat: add ValidPassword annotation for password validation

---
+++ src/main/java/com/rpg/rpghxh/business/validation/ValidPassword.java
@@ -0,0 +1,20 @@
+package com.rpg.rpghxh.business.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+
+    String message() default "A senha deve ter no mínimo 8 caracteres, incluindo pelo menos uma letra maiúscula, uma minúscula e um caractere especial";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
