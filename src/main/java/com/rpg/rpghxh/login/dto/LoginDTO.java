9558bf45417e3536ebd9cff9d6163e1ea5328b0b
Art <paganotiarthur@gmail.com>
2026-03-22 00:29:00 -0300

    :sparkles: feat: add LoginDTO class for user login data transfer

---
+++ src/main/java/com/rpg/rpghxh/login/dto/LoginDTO.java
@@ -0,0 +1,22 @@
+package com.rpg.rpghxh.login.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginDTO {
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "O email deve ser válido")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    private String senha;
+}
