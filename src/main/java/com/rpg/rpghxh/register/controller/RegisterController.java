05f52bcebfa16ab94dea13fda81486d5a62a3c4d
Art <paganotiarthur@gmail.com>
2026-03-22 00:25:22 -0300

    :recycle: refactor: remove unnecessary user request mapping from RegisterController

--- src/main/java/com/rpg/rpghxh/register/controller/RegisterController.java
+++ src/main/java/com/rpg/rpghxh/register/controller/RegisterController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
 @Tag(name = "Usuários", description = "Gerenciamento de usuários")
 public class RegisterController {
 
