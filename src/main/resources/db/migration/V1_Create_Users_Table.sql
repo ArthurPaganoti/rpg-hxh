6c14681616fd651a193905da6ea525fdeb82539d
Art <paganotiarthur@gmail.com>
2026-03-24 22:50:16 -0300

    :fire: remove V1_Create_Users_Table.sql database migration script

--- src/main/resources/db/migration/V1_Create_Users_Table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-);
